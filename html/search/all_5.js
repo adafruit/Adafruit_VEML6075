067c84d9f2d43d4d2f449d8bef04d8a128baa816
Doxygen CI <ci-arduino@invalid>
2021-07-14 20:20:53 +0000

    Deploy docs to GitHub Pages from commit a53b62f7e4
    Commit: a53b62f7e4104ea7a033797dfc9f057fe6cce061
    GitHub Actions run: 1031586049

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['readuva',['readUVA',['../class_adafruit___v_e_m_l6075.html#a8b61f7be04c52735485b529d9314e311',1,'Adafruit_VEML6075']]],
+  ['readuvabi',['readUVABI',['../class_adafruit___v_e_m_l6075.html#a9d70587d67c88abcdcfd1f44b45416df',1,'Adafruit_VEML6075']]],
   ['readuvb',['readUVB',['../class_adafruit___v_e_m_l6075.html#a8ae68ea5254c31bb0d11ac0b60b87ffa',1,'Adafruit_VEML6075']]],
   ['readuvi',['readUVI',['../class_adafruit___v_e_m_l6075.html#a88e38a34fe6a7df3e3dbdc5b41a22640',1,'Adafruit_VEML6075']]],
   ['reg',['reg',['../unionveml6075__command_register.html#a6b4225b643bf4651bc6ae59b3e90957e',1,'veml6075_commandRegister']]]
